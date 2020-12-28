8873b7e1a37ece00c418adcba5faa5569bb6c8ab
Tierry Hörmann <tierryh@student.ethz.ch>
2020-12-28 16:16:43 +0100

    Added safe register for gym environment

--- minisat/__init__.py
+++ minisat/__init__.py
@@ -27,7 +27,20 @@
 
 from .minisat.gym.MiniSATEnv import gym_sat_Env
 
-from gym.envs.registration import register
+import gym
+
+
+def register(id, entry_point, force=True):
+    env_specs = gym.envs.registry.env_specs
+    if id in env_specs.keys():
+        if not force:
+            return
+        del env_specs[id]
+    gym.register(
+        id=id,
+        entry_point=entry_point,
+    )
+
 
 # Graph-Q-SAT UPD: register the sat environment
 register(id="sat-v0", entry_point="minisat.minisat.gym.MiniSATEnv:gym_sat_Env")
